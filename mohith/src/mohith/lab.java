de858aca45cd751a878361236a56620d8cacef1b
AdmiN <AdmiN@ITWS0052>
2024-08-29 10:32:06 +0530

    yes

---
+++ mohith/src/mohith/lab.java
@@ -0,0 +1,10 @@
+package mohith;
+
+public class lab {
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+		System.out.println("shynesh,vijaya_lakshmi,mohith_reddy");
+	}
+
+}
